00e3c25c375c2c10a97dc4d63b2d64d95b6f95dc
Matheesan <mathee25@hotmail.com>
2018-11-12 01:05:40 -0500

    setting up mid-way

---
+++ test.py
@@ -0,0 +1,36 @@
+import re
+import numpy as numpy
+import pandas as pd
+from pprint import pprint 
+
+#Gensim
+import Gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+#Spacy for lemmatization
+import spacy
+
+#Plotting tools
+import pyLDAvis
+import pyLDAvis.gensim #Important
+import matplotlib.pyplot as plt 
+%matplotlib inline
+
+#Enable logging for gensim
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+#NLTK stop words
+from nltk.corpus import stopwords
+stop_words= stopwords.words('english')
+stop_words.extend(['from','subject','re','edu','use'])
+
+#Import Dataset
+df = pd.read_json(https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
+print(df.target_names.unique())
+df.head()
